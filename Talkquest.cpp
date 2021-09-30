377ae2830f4297d2389596174dd93614b01416e6
JoshuaJWatt <48531152+JoshuaJWatt@users.noreply.github.com>
2021-09-30 10:34:52 +0100

    Update Talkquest.cpp

--- Talkquest.cpp
+++ Talkquest.cpp
@@ -18,30 +18,26 @@ class cha_cla
     vector<int> points = {0, 10, 20, 50, 100, 150, 200, 500, 1000, 1500, 2000, 3000, 10000};
     vector<string> canned = {"Elf Stranger: Nice to hear it.\n", "Elf Stranger: Really?\n", "Elf Stranger: Tell me more.\n", "Elf Stranger: You go girlfriend\n", "Elf Stranger: Totes emosh\n", "Elf Stranger: The important thing is you tried\n", "Elf Stranger: Wow, that's so insightful :^)\n"};
 
-    string choose_string (vector<string> pickvec)
-    {
-            int random_index = rand() % pickvec.size();
-            string result_str;
-            result_str = pickvec[random_index];
-            return result_str;
+    string choose_string (vector<string> pickvec){
+        int random_index = rand() % pickvec.size();
+        string result_str;
+        result_str = pickvec[random_index];
+        return result_str;
     }
-    string incr ()
-    {
+    string incr (){
         int rm = rand() % 9 + 1;
         exp += rm;
         string upd8;
         upd8 = "You have gained " + std::to_string(rm) + " experience point(s) and now have " + std::to_string(exp) +". ";
         return upd8;
     }
-    string intera ()
-    {
+    string intera (){
         string vo;
         cout << nam << ": ";
         cin >> vo;
         cout << choose_string(canned) << endl;
         cout << incr() << endl;
-        if (points[lvl]<exp )
-        {
+        if (points[lvl]<exp ){
             lvl++;
             cout << nam << " gained a level and is now level " << lvl << "!";
         }
